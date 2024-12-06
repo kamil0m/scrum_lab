0211a91f31c80d0e33ab9c745ba30fa5837f2583
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-06 10:33:41 +0100

    project copied from private repository

---
+++ development/js/index.js
@@ -0,0 +1,7 @@
+import Slider from "./components/slider";
+
+
+const slides = document.querySelectorAll('#slider .slide');
+const arrows = document.querySelectorAll('.arrow');
+
+Slider(slides, arrows);
