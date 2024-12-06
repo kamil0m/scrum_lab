e0f3fa44094adea052ad8eba92a21ef050267381
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-06 10:39:46 +0100

    project initiazed

--- development/js/index.js
+++
@@ -1,7 +0,0 @@
-import Slider from "./components/slider";
-
-
-const slides = document.querySelectorAll('#slider .slide');
-const arrows = document.querySelectorAll('.arrow');
-
-Slider(slides, arrows);
